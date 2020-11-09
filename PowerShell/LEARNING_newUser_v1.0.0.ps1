ffc6b6d9bb9b7b7433a483fa7c2126c211ec98d0
Administrator@LABORATORY.INT <Administrator@LABORATORY.INT>
2020-11-09 22:16:16 +0100

    20201109_2216

--- PowerShell/LEARNING_newUser_v1.0.0.ps1
+++ PowerShell/LEARNING_newUser_v1.0.0.ps1
@@ -1,3 +1,4 @@
+﻿Clear-Host
 $pdc_server = (Get-ADDomain).PDCEmulator
 $domain_name = (Get-ADDomain).DNSRoot
 $ou_path = "OU=STANDARD,OU=USERS,OU=LABORATORY,DC=laboratory,DC=int"
@@ -7,9 +8,18 @@ $last_name = Read-Host -Prompt "Nazwisko"
 $login = Read-Host -Prompt "Login"
 $user_password = Read-Host -AsSecureString -Prompt "Hasło"
 $expiration_date = Read-Host -Prompt "Data ważności (yyyy-mm-dd)"
+try {
+    $account_expiration = ([datetime]::ParseExact($expiration_date, "yyyy-MM-dd", $null)).ToString()
+}
+catch [FormatException] {
+    Write-Host "Wrong DateTime format, it should be (yyyy-MM-dd)" -ForegroundColor Red
+    exit
+}
+catch {
+    Write-Output $_
+    exit
+}
 $user_description = Read-Host -Prompt "Opis konta"
-$account_expiration = ([datetime]::ParseExact($expiration_date, "yyyy-MM-dd", $null)).ToString()
-
 $user_name = "$first_name $last_name"
 
 $user_upn = "$login@$domain_name"
@@ -35,4 +45,5 @@ Set-ADUser -ChangePasswordAtLogon $true `
     -Identity $login `
     -Server $pdc_server `
     -SmartcardLogonRequired $false `
-    -Description $user_description
+    -Description $user_description `
+    -EmailAddress $user_upn
