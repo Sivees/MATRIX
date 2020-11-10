340d944c0c035ac54c6a77172007ca57b2955c7a
Administrator@LABORATORY.INT <Administrator@LABORATORY.INT>
2020-11-10 18:50:18 +0100

    20201110_1850

--- PowerShell/LEARNING_newUser_v1.0.0.ps1
+++ PowerShell/LEARNING_newUser_v1.0.0.ps1
@@ -1,39 +1,78 @@
 ﻿function New-LabUser {
+    <#
+    .SYNOPSIS
+
+        Addong new user in Laboratory domain.
+
+    .DESCRIPTION
+
+    Adds new user .
+    Takes any strings for the file name or extension.
+
+    .PARAMETER Name
+    Specifies the file name.
+
+    .PARAMETER Extension
+    Specifies the extension. "Txt" is the default.
+
+    .INPUTS
+
+    None. You cannot pipe objects to Add-Extension.
+
+    .OUTPUTS
+
+    System.String. Add-Extension returns a string with the extension
+    or file name.
+
+    .EXAMPLE
+
+    PS> extension -name "File"
+    File.txt
+
+    .EXAMPLE
+
+    PS> extension -name "File" -extension "doc"
+    File.doc
+
+    .EXAMPLE
+
+    PS> extension "File" "doc"
+    File.doc
+
+    .LINK
+
+    http://www.fabrikam.com/extension.html
+
+    .LINK
+
+    Set-Item
+    #>
     [CmdletBinding()]
     param (
-        # Parameter help description
         [Parameter()]
         [string]
         $Server=(Get-ADDomain).PDCEmulator,
-        # Parameter help description
         [Parameter(Mandatory=$true)]
         [string]
         $Path,
-        # Parameter help description
         [Parameter(Mandatory)]
         [string]
         $FirstName,
-        # Parameter help description
         [Parameter(Mandatory)]
         [string]
         $LastName,
-        # Parameter help description
         [Parameter(Mandatory)]
         [string]
         $SamAccountName,
-        # Parameter help description
         [Parameter(Mandatory)]
         [securestring]
         $Password,
-        # Parameter help description
         [Parameter(Mandatory)]
         [string]
         $AccountExpiration,
-        # Parameter help description
         [Parameter(Mandatory)]
         [string]
         $Description,
-        # Parameter help description
         [string]
         $Country = "PL"
     )
