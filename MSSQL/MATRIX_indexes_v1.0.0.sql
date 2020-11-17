3dae1000fcc97956a722a654cbcb95a5492f0dcf
Administrator@LABORATORY.INT <Administrator@LABORATORY.INT>
2020-11-17 20:11:56 +0100

    20201117_2011

--- MSSQL/MATRIX_indexes_v1.0.0.sql
+++ MSSQL/MATRIX_indexes_v1.0.0.sql
@@ -5,3 +5,4 @@ CREATE INDEX idx_nation_polish_name ON tbl_nation(polish_name);
 CREATE INDEX idx_military_rank_hierarchy ON tbl_military_rank(hierarchy);
 CREATE INDEX idx_person_last_name ON tbl_person(last_name);
 CREATE INDEX idx_person_pesel ON tbl_person(pesel);
+CREATE UNIQUE INDEX idx_person_pesel_unique ON tbl_person(pesel);
