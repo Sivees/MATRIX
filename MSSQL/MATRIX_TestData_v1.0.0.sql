b5b15dd613bc1a5c55bb55e733de111d070cfe9d
Administrator@LABORATORY.INT <Administrator@LABORATORY.INT>
2020-11-19 17:35:32 +0100

    20201119_1735

--- MSSQL/MATRIX_TestData_v1.0.0.sql
+++ MSSQL/MATRIX_TestData_v1.0.0.sql
@@ -21,7 +21,7 @@ VALUES (
 	'66E7336E-FA16-EB11-995E-000C295890EE', --military rank
 	'John',
 	'Doe',
-	NULL,
+	'USA00000001',
 	CONVERT(date, '1980-05-04')
 )
 GO
@@ -53,7 +53,7 @@ GO
 --insert identity documents
 INSERT INTO tbl_identity_document (id_person, id_identity_document_type, serial_number, date_of_issue, expiration_date, issuer)
 VALUES (
-	'C47DD798-B629-EB11-9965-000C2907CAFC', --person
+	(SELECT id_person FROM tbl_person WHERE pesel = '00000000002'), --person
 	'C8C28E99-F716-EB11-995E-000C295890EE', --id type
 	'127/2010',
 	CONVERT(date, '2000-05-04'),
@@ -63,7 +63,7 @@ VALUES (
 
 INSERT INTO tbl_identity_document (id_person, id_identity_document_type, serial_number, date_of_issue, expiration_date, issuer)
 VALUES (
-	'C47DD798-B629-EB11-9965-000C2907CAFC', --person
+	(SELECT id_person FROM tbl_person WHERE pesel = '00000000002'), --person
 	'C8C28E99-F716-EB11-995E-000C295890EE', --id type
 	'127/2010',
 	CONVERT(date, '2005-05-04'),
@@ -73,7 +73,7 @@ VALUES (
 
 INSERT INTO tbl_identity_document (id_person, id_identity_document_type, serial_number, date_of_issue, expiration_date, issuer)
 VALUES (
-	'C77DD798-B629-EB11-9965-000C2907CAFC', --person
+	(SELECT id_person FROM tbl_person WHERE pesel = '00000000003'), --person
 	'C8C28E99-F716-EB11-995E-000C295890EE', --id type
 	'3/1920',
 	CONVERT(date, '1920-05-04'),
@@ -83,7 +83,7 @@ VALUES (
 
 INSERT INTO tbl_identity_document (id_person, id_identity_document_type, serial_number, date_of_issue, expiration_date, issuer)
 VALUES (
-	'C77DD798-B629-EB11-9965-000C2907CAFC', --person
+	(SELECT id_person FROM tbl_person WHERE pesel = '00000000003'), --person
 	'C8C28E99-F716-EB11-995E-000C295890EE', --id type
 	'18/1925',
 	CONVERT(date, '1925-05-04'),
@@ -93,7 +93,7 @@ VALUES (
 
 INSERT INTO tbl_identity_document (id_person, id_identity_document_type, serial_number, date_of_issue, expiration_date, issuer)
 VALUES (
-	'C77DD798-B629-EB11-9965-000C2907CAFC', --person
+	(SELECT id_person FROM tbl_person WHERE pesel = '00000000003'), --person
 	'C7C28E99-F716-EB11-995E-000C295890EE', --id type
 	'12/1926',
 	CONVERT(date, '1926-05-04'),
@@ -102,4 +102,34 @@ VALUES (
 )
 
 --insert security clearances
-INSERT INTO tbl_security_clearance (id_person, id_security_clearance_type, )
+INSERT INTO tbl_security_clearance (id_person, id_security_clearance_type, serial_number, date_of_issue, issuer)
+VALUES (
+	(SELECT id_person FROM tbl_person WHERE pesel = '00000000002'), --person
+	'EF75A0CD-FE16-EB11-995E-000C295890EE',
+	'78/2015',
+	CONVERT(date, '2005-05-04'),
+	'Agencja Bezpieczeństwa Wewnętrznego'
+)
+
+INSERT INTO tbl_clearance_classification (id_security_clearance, id_security_classification, expiration_date)
+VALUES (
+	(SELECT id_security_clearance FROM tbl_security_clearance WHERE serial_number = '78/2015'),
+	'88B3561F-FE16-EB11-995E-000C295890EE',
+	(SELECT DATEADD(DAY, -1, DATEADD(YEAR, 5, date_of_issue)) FROM tbl_security_clearance)
+)
+
+INSERT INTO tbl_clearance_classification (id_security_clearance, id_security_classification, expiration_date)
+VALUES (
+	(SELECT id_security_clearance FROM tbl_security_clearance WHERE serial_number = '78/2015'),
+	'87B3561F-FE16-EB11-995E-000C295890EE',
+	(SELECT DATEADD(DAY, -1, DATEADD(YEAR, 7, date_of_issue)) FROM tbl_security_clearance)
+)
+
+INSERT INTO tbl_clearance_classification (id_security_clearance, id_security_classification, expiration_date)
+VALUES (
+	(SELECT id_security_clearance FROM tbl_security_clearance WHERE serial_number = '78/2015'),
+	'86B3561F-FE16-EB11-995E-000C295890EE',
+	(SELECT DATEADD(DAY, -1, DATEADD(YEAR, 7, date_of_issue)) FROM tbl_security_clearance)
+)
+
+---
