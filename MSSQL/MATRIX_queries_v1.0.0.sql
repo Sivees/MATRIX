9a3d67219e997915ed41ff77a7f4050c3d970f9b
Administrator@LABORATORY.INT <Administrator@LABORATORY.INT>
2020-11-20 16:02:17 +0100

    20201120_1602

--- MSSQL/MATRIX_queries_v1.0.0.sql
+++ MSSQL/MATRIX_queries_v1.0.0.sql
@@ -1,35 +1,19 @@
 ﻿USE MATRIX
 GO
 
-SELECT CONCAT_WS(' ', tmr.short_name, tp.first_name, tp.last_name) AS Person,
-	tp.pesel AS PESEL,
-	tidt.full_name AS DokumentTożsamości,
-	tid.serial_number AS NumerSeryjny,
-	tid.expiration_date AS DataWażności
-FROM tbl_person AS tp
+SELECT CONCAT_WS(' ', tmr.short_name, tp.first_name, tp.last_name) AS person,
+	tsct.full_name AS ClearanceType,
+	tss.full_name AS SecurityState,
+	tsc.full_name AS SecurityClassification,
+	vgnsc.issuer AS ClearanceIssuer,
+	vgnsc.serial_number AS ClearanceSerialNumber,
+	vgnsc.date_of_issue AS ClearanceDateOfIssue,
+	vgnsc.expiration_date AS ClearanceExpirationDate
+FROM viw_get_newest_security_clearance AS vgnsc
+	LEFT JOIN tbl_person AS tp ON vgnsc.id_person = tp.id_person
+	LEFT JOIN tbl_security_classification AS tsc ON vgnsc.id_security_classification = tsc.id_security_classification
+	LEFT JOIN tbl_security_state AS tss ON vgnsc.id_security_state = tss.id_security_state
+	LEFT JOIN tbl_security_clearance_type AS tsct ON vgnsc.id_security_clearance_type = tsct.id_security_clearance_type
 	LEFT JOIN tbl_military_rank AS tmr ON tp.id_military_rank = tmr.id_military_rank
-	LEFT JOIN tbl_identity_document AS tid ON tp.id_person = tid.id_person
-	LEFT JOIN tbl_identity_document_type AS tidt ON tid.id_identity_document_type = tidt.id_identity_document_type
-ORDER BY tp.id_person, tidt.id_identity_document_type, tid.expiration_date DESC
-
-SELECT *
-FROM tbl_identity_document AS tid
-	INNER JOIN tbl_identity_document_type AS tidt ON tid.id_identity_document_type = tidt.id_identity_document_type
-ORDER BY tid.id_person, tid.id_identity_document_type, tid.expiration_date DESC
-
-SELECT DISTINCT id_person, id_identity_document_type
-FROM tbl_identity_document
-
-
-WITH cte_identity_document AS(
-	SELECT *,
-		ROW_NUMBER() OVER(PARTITION BY id_person, id_identity_document_type ORDER BY expiration_date DESC) AS row_no
-	FROM tbl_identity_document)
-SELECT id_identity_document,
-	id_identity_document_type,
-	id_person, serial_number,
-	date_of_issue, expiration_date,
-	issuer, is_active,
-	creation_time
-FROM cte_identity_document
-WHERE row_no = 1
+WHERE vgnsc.expiration_date <= DATEADD(YEAR, 6, GETDATE())
+ORDER BY tp.last_name
