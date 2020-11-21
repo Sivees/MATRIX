575bb009205cc9d050ff79d9459b53e8ec2ecc42
Administrator@LABORATORY.INT <Administrator@LABORATORY.INT>
2020-11-21 12:01:15 +0100

    20201121_1201

--- MSSQL/MATRIX_queries_v1.0.0.sql
+++ MSSQL/MATRIX_queries_v1.0.0.sql
@@ -9,6 +9,7 @@ FROM tbl_person AS tp
 	LEFT JOIN tbl_sex AS ts ON tp.id_sex = ts.id_sex
 	LEFT JOIN tbl_military_rank AS tmr ON tp.id_military_rank = tmr.id_military_rank
 	LEFT JOIN tbl_nato_rank AS tnr ON tmr.id_nato_rank = tnr.id_nato_rank
+ORDER BY Sex
 GO
 
 WITH cte_person AS (
@@ -23,7 +24,7 @@ FROM cte_person AS cp
 	INNER JOIN tbl_sex AS ts ON cp.id_sex = ts.id_sex
 GO
 
-SELECT CONCAT_WS(' ', tmr.short_name, tp.first_name, UPPER(tp.last_name)) AS Person,
+SELECT CONCAT_WS(' ', tnr.short_name, tp.first_name, UPPER(tp.last_name)) AS Person,
 	tsct.full_name AS ClearanceType,
 	tss.full_name AS SecurityState,
 	tsc.full_name AS SecurityClassification,
@@ -37,6 +38,7 @@ FROM viw_get_newest_security_clearance AS vgnsc
 	LEFT JOIN tbl_security_state AS tss ON vgnsc.id_security_state = tss.id_security_state
 	LEFT JOIN tbl_security_clearance_type AS tsct ON vgnsc.id_security_clearance_type = tsct.id_security_clearance_type
 	LEFT JOIN tbl_military_rank AS tmr ON tp.id_military_rank = tmr.id_military_rank
+	LEFT JOIN tbl_nato_rank AS tnr ON tmr.id_nato_rank = tnr.id_nato_rank
 --WHERE vgnsc.expiration_date <= DATEADD(YEAR, 6, GETDATE())
 ORDER BY tp.last_name, tsct.id_security_clearance_type, tss.id_security_state, tsc.hierarchy DESC
 GO
--- PowerShell/LEARNING_base_v1.0.0.ps1
+++ PowerShell/LEARNING_base_v1.0.0.ps1
@@ -23,3 +23,15 @@ Add-ADGroupMember -Server $pdc_server -Identity 'LAB_MSQL_Servers' -Members 'ALP
 # Wykreowanie klucza KDS z z utworzeniem konta serwisowego dla MSSQL
 # Add-KdsRootKey -EffectiveTime ((get-date).addhours(-10))
 # New-ADServiceAccount -Server $pdc_server -Name 'LAB_MSQL_SVC' -DNSHostName $pdc_server -PrincipalsAllowedToRetrieveManagedPassword 'LAB_MSQL_Servers'
+
+New-SelfSignedCertificate -Type Custom -Subject "E=administrator@laboratory.int,CN=Administrator" `
+    -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.4","2.5.29.17={text}email=administrator@laboratory.int&upn=administrator@laboratory.int") `
+    -KeyAlgorithm ECDSA_nistP384 `
+    -HashAlgorithm sha384 `
+    -SmimeCapabilities `
+    -CertStoreLocation "Cert:\CurrentUser\My" `
+    -KeyUsage DigitalSignature 
+
+Set-ADObject -Identity "CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,DC=laboratory,DC=int" `
+    -Partition "CN=Configuration,DC=laboratory,DC=int" `
+    -Replace:@{"msDS-DeletedObjectLifetime" = 60}
