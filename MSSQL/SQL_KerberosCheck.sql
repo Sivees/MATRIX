7c7b95b5fa60a431d23ea494be43d5892440ff4f
Administrator@LABORATORY.INT <Administrator@LABORATORY.INT>
2020-11-22 13:03:24 +0100

    20201122_1303

--- MSSQL/SQL_KerberosCheck.sql
+++ MSSQL/SQL_KerberosCheck.sql
@@ -3,4 +3,5 @@ GO
 
 SELECT S.login_name, C.net_transport, C.auth_scheme, s.host_name, s.program_name
 FROM sys.dm_exec_connections AS C
-JOIN sys.dm_exec_sessions AS S ON C.session_id = S.session_id;
+JOIN sys.dm_exec_sessions AS S ON C.session_id = S.session_id
+ORDER BY net_transport
